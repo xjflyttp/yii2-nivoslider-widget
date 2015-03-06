b897643ae13bdd11ef925f79896859a048a07687
xjflyttp <xjflyttp@gmail.com>
2015-03-06 16:03:26 +0800

    update

--- SilderAsset.php
+++ SilderAsset.php
@@ -12,25 +12,10 @@ use yii\web\AssetBundle;
 
 class SilderAsset extends AssetBundle {
 
-    public $sourcePath;
+    public $sourcePath = '@vendor/xj/yii2-nivoslider-widget/assets';
     public $basePath = '@webroot/assets';
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
     public $css = ['nivo-slider.css'];
-    public $js = [];
+    public $js = ['jquery.nivo.slider.js'];
     public $depends = ['yii\web\JqueryAsset'];
-
-    private function getJs() {
-        return [
-            YII_DEBUG ? 'jquery.nivo.slider.js' : 'jquery.nivo.slider.pack.js',
-        ];
-    }
-
-    public function init() {
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
-        if (empty($this->js)) {
-            $this->js = $this->getJs();
-        }
-        return parent::init();
-    }
 
 }

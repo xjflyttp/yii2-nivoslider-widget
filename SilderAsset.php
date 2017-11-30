6aa180142246ec969e5a45dfb5a00023abcdeb2d
xjflyttp <xjflyttp@gmail.com>
2017-11-30 10:57:49 +0800

    fix basePath

--- SilderAsset.php
+++ SilderAsset.php
@@ -13,7 +13,6 @@ use yii\web\AssetBundle;
 class SilderAsset extends AssetBundle {
 
     public $sourcePath = '@vendor/xj/yii2-nivoslider-widget/assets';
-    public $basePath = '@webroot/assets';
     public $css = ['nivo-slider.css'];
     public $js = ['jquery.nivo.slider.js'];
     public $depends = ['yii\web\JqueryAsset'];
